ca90d0da4f88ec77b66013f033c8760c8d85eb6c
sourabh.kumar <Sourabh1998>
2021-04-11 08:50:06 +0530

    server code separated

--- frontend/src/app/round-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoundPipePipe } from './round-pipe.pipe';
-
-describe('RoundPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new RoundPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
